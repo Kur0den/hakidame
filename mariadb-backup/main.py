0e41bb66a4441ef72ddd6f4aceb809de7efe8f77
Kur0den <git@kur0den.net>
2025-03-06 17:13:11 +0900

    MariaDBのバックアップスクリプトを追加
    多分動く

---
+++ mariadb-backup/main.py
@@ -0,0 +1,73 @@
+import boto3
+import os
+import dotenv
+import subprocess
+import logging
+import datetime
+
+# .envファイルの読み込み
+dotenv.load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+
+def upload_to_s3(file_path, object_name):
+
+    # 環境変数の取得
+    S3_ENDPOINT = os.getenv('S3_ENDPOINT')
+    S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+    S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
+    S3_BUCKET = os.getenv('S3_BUCKET')
+
+    # boto3クライアントの作成
+    s3_client = boto3.client('s3',
+                            endpoint_url=S3_ENDPOINT,
+                            aws_access_key_id=S3_ACCESS_KEY,
+                            aws_secret_access_key=S3_SECRET_KEY)
+
+    # ファイルのアップロード
+    try:
+        s3_client.upload_file(file_path, S3_BUCKET, object_name)
+        logging.info(f"File {file_path} uploaded to {S3_BUCKET}/{object_name}")
+        return
+    except Exception as e:
+        logging.error(f"Error uploading file: {e}")
+        raise e
+
+def dump_db(BACKUP_DIR):
+    # 環境変数の取得
+    DB_HOST = os.getenv('MARIADB_HOST')
+    DB_PORT = os.getenv('MARIADB_PORT')
+    DB_USER = os.getenv('MARIADB_USER')
+    DB_PASS = os.getenv('MARIADB_PASS')
+    DB_NAME = os.getenv('MARIADB_DATABASE')
+
+
+
+    # バックアップファイルのパス
+    backup_file = os.path.join(BACKUP_DIR, "mariadb_backup",  f"{DB_NAME}.sql")
+
+    # mysqldumpコマンドの実行
+    try:
+        subprocess.run([
+            'mysqldump',
+            f'--host={DB_HOST}',
+            f'--port={DB_PORT}',
+            f'--user={DB_USER}',
+            f'--password={DB_PASS}',
+            DB_NAME,
+            f'--result-file={backup_file}'
+        ], check=True)
+        logging.info(f"Database backup saved to {backup_file}")
+        return backup_file
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error during database backup: {e}")
+        raise e
+
+def main():
+    # バックアップファイルの保存先
+    # $XDG_CACHE_HOMEが設定されていない場合は$HOME/.cacheに保存
+    BACKUP_DIR = os.getenv('XDG_CACHE_HOME', os.getenv('HOME') + '/.cache')
+    backup_file = dump_db(BACKUP_DIR)
+
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    upload_to_r2(backup_file, f"mariadb_backup/{date}.sql")
---
+++ mariadb-backup/readme.md
@@ -0,0 +1,61 @@
+# MariaDB Backup
+
+このプロジェクトは、MariaDBデータベースのバックアップを作成し、S3互換のオブジェクトストレージにアップロードするPythonスクリプトです。
+
+スクリプトが実行されると、最終的に`S3_BUCKET`で指定したバケット内に`mariadb_backup/{date}.sql`という形式でアップロードされます
+
+## 必要条件
+
+- Python 3.x
+- `boto3` ライブラリ
+- `python-dotenv` ライブラリ
+- `mysqldump` コマンド
+
+## インストール
+
+必要なPythonライブラリをインストールします。
+
+```bash
+pip install -r requirements.txt
+```
+
+## 環境変数の設定
+
+プロジェクトのルートディレクトリに `.env.example` ファイルをコピーし `.env` ファイルを作成し、以下の環境変数を設定します。
+
+```env
+S3_ENDPOINT=https://example.com
+S3_ACCESS_KEY=your-access-key
+S3_SECRET_KEY=your-secret-key
+S3_BUCKET=your-bucket
+
+MARIADB_HOST=localhost
+MARIADB_PORT=3306
+MARIADB_USER=root
+MARIADB_PASS=root
+MARIADB_DATABASE=database
+```
+
+## 使用方法
+
+スクリプトを実行して、データベースのバックアップを作成し、S3互換のオブジェクトストレージにアップロードします。
+
+```bash
+python3 main.py
+```
+
+## スクリプトの説明
+
+### `main.py`
+
+- `.env` ファイルを読み込み、環境変数を設定します。
+- `dump_db` 関数でデータベースのバックアップを作成します。
+- `upload_to_S3` 関数でバックアップファイルをS3互換のオブジェクトストレージにアップロードします。
+
+### `.env.example`
+
+環境変数の例を示すファイルです。必要に応じて `.env` ファイルを作成してください。
+
+## ライセンス
+
+このプロジェクトはMITライセンスの下で公開されています。
