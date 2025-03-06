261ccfd6f909fe7b5e6af3ac9adb82ab9c296bb9
Kur0den <git@kur0den.net>
2025-03-06 17:34:54 +0900

    いろいろ修正

--- mariadb-backup/main.py
+++ mariadb-backup/main.py
@@ -1,12 +1,12 @@
 import boto3
 import os
-import dotenv
+from dotenv import load_dotenv
 import subprocess
 import logging
 import datetime
 
 # .envファイルの読み込み
-dotenv.load_dotenv()
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,6 +17,11 @@ def upload_to_s3(file_path, object_name):
     S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
     S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
     S3_BUCKET = os.getenv('S3_BUCKET')
+
+    # 未定義の環境変数がある場合はエラー
+    if None in (S3_ENDPOINT, S3_ACCESS_KEY, S3_SECRET_KEY, S3_BUCKET):
+        logging.error("Environment variables not set")
+        raise ValueError("Environment variables not set")
 
     # boto3クライアントの作成
     s3_client = boto3.client('s3',
@@ -40,6 +45,11 @@ def dump_db(BACKUP_DIR):
     DB_USER = os.getenv('MARIADB_USER')
     DB_PASS = os.getenv('MARIADB_PASS')
     DB_NAME = os.getenv('MARIADB_DATABASE')
+
+    # 未定義の環境変数がある場合はエラー
+    if None in (DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME):
+        logging.error("Environment variables not set")
+        raise ValueError("Environment variables not set")
 
 
 
@@ -66,8 +76,9 @@ def dump_db(BACKUP_DIR):
 def main():
     # バックアップファイルの保存先
     # $XDG_CACHE_HOMEが設定されていない場合は$HOME/.cacheに保存
-    BACKUP_DIR = os.getenv('XDG_CACHE_HOME', os.getenv('HOME') + '/.cache')
+    BACKUP_DIR = os.getenv('XDG_CACHE_HOME', os.getenv('HOME', "~") + '/.cache')
     backup_file = dump_db(BACKUP_DIR)
 
+
     date = datetime.datetime.now().strftime("%Y-%m-%d")
-    upload_to_r2(backup_file, f"mariadb_backup/{date}.sql")
+    upload_to_s3(backup_file, f"mariadb_backup/{date}.sql")
